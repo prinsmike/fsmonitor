6c0d95129bc0f74c12c6cbd7e5c8c3c9a731c6b7
Michael Prinsloo <mike.prinsloo@gmail.com>
2013-02-09 22:36:43 +0200

    Loading config.

--- fsmonitor.go
+++ fsmonitor.go
@@ -10,3 +10,13 @@ import (
 func main() {
 
 }
+
+func loadConfig() interface{} {
+	// Read the config file.
+	var v interface{}
+	f, _ := os.Open("~/.fsmonitor.rc")
+	// wrap our reader before passing it to the json decoder
+	r := JsonConfigReader.New(f)
+	json.NewDecoder(r).Decode(&v)
+	return v
+}
